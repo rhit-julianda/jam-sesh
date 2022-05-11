a46745025a6f79b75d8ea11a87df88fef946d018
rhit-julianda <julianda@rose-hulman.edu>
2022-05-11 18:25:06 -0400

    added sound and dimension scaling to pianoview

--- Frontend/jam-sesh/src/Components/PianoView.js
+++ Frontend/jam-sesh/src/Components/PianoView.js
@@ -1,27 +1,39 @@
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
+import DimensionsProvider from './DimensionsProvider';
+import SoundfontProvider from './SoundfontProvider';
 
 function PianoView() {
-    const firstNote = MidiNumbers.fromNote('c3');
-    const lastNote = MidiNumbers.fromNote('f5');
+    const firstNote = MidiNumbers.fromNote('c4');
+    const lastNote = MidiNumbers.fromNote('g6');
     const keyboardShortcuts = KeyboardShortcuts.create({
       firstNote: firstNote,
       lastNote: lastNote,
       keyboardConfig: KeyboardShortcuts.HOME_ROW,
     });
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
    
     return (
-      <Piano
-        noteRange={{ first: firstNote, last: lastNote }}
-        playNote={(midiNumber) => {
-          // Play a given note - see notes below
-        }}
-        stopNote={(midiNumber) => {
-          // Stop playing a given note - see notes below
-        }}
-        width={1500}
-        keyboardShortcuts={keyboardShortcuts}
-      />
+      <DimensionsProvider>
+        {({ containerWidth, containerHeight }) => (
+          <SoundfontProvider
+            instrumentName="acoustic_grand_piano"
+            audioContext={audioContext}
+            hostname={soundfontHostname}
+            render={({ isLoading, playNote, stopNote }) => (
+              <Piano
+                noteRange={{first: firstNote, last: lastNote}}
+                width={containerWidth}
+                playNote={playNote}
+                stopNote={stopNote}
+                disabled={isLoading}
+                keyboardShortcuts={keyboardShortcuts}
+              />
+            )}
+          />
+        )}
+      </DimensionsProvider>
     );
   }
 
