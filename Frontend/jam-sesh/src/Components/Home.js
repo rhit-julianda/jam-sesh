fc17788e820cef676784d2664cb1f4d9b1193321
Costa <costarr@rose-hulman.edu>
2022-05-11 18:28:35 -0400

    session, userlist, and home structure set up

--- Frontend/jam-sesh/src/Components/Home.js
+++ Frontend/jam-sesh/src/Components/Home.js
@@ -1,16 +1,36 @@
 import TitleBar from './TitleBar'
 import PianoView from './PianoView';
-import Session from './Session';
 import './Styles/Home.css'
+import React from 'react';
 
-function Home() {
-  return (
+class Home extends React.Component{
+  constructor(){
+    super();
+    this.target = "";
+  }
+
+  changeTarget(e){
+    this.target = e;
+  }
+
+  render(){
+    return (
       <div>
       	<TitleBar />
       	<h1>Home</h1>
-        <PianoView />
+        <p> Join Code: {'  '}
+        <input
+            type="text"
+            id="joinCodeInput"
+            onChange={(text) => this.changeTarget(text)}
+            readOnly = {false}
+         /> {'      '}
+        <button>Join</button>
+         </p>
+         <p></p>
       </div>
-  );
+    );
+  }
 }
 
 export default Home;
--- Frontend/jam-sesh/src/Components/Session.js
+++ Frontend/jam-sesh/src/Components/Session.js
@@ -1,22 +1,26 @@
 import PianoView from "./PianoView";
 import UserList from "./UserList";
+import React from "react";
 
-class Session{
+class Session extends React.Component{
   constructor(props){
+    super(props);
     this.joinCode = props.code;
+    this.list = new UserList;
+    this.list.addUser('E');
   }
 
   render(){
-  return (
-      <div>
+  return( 
+  <div>
       <PianoView />
       <p>
       <h1>Session</h1>
       <h1>Join Code: {this.joinCode}</h1>
       </p>
-      <UserList />
+      <div>{this.list.render()}</div>
       </div>
-  );
+  )
   }
 }
 
